23c7491cba01acb1ddebbb71dbcdf98bb67a8969
Mahidhar5757 <168r1a04d9@gmail.com>
2025-12-27 08:49:10 +0530

    Add Task 1 and Task 2 Python files

---
+++ Task_2.py
@@ -0,0 +1,12 @@
+N1 = int(input("Enter the first number: "))
+N2 = int(input("Enter the second number: "))
+
+Addition = N1 + N2
+Subtraction = N1 - N2
+Division = N1 / N2
+Multiplication = N1 * N2
+
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
